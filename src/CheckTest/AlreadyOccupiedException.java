e4b6038475e973baae70efa919079fe74bf69dd7
Dominic Peart <dominicpeart@dominics-imac.home>
2017-12-11 13:35:29 +0000

    Initial commit

---
+++ src/CheckTest/AlreadyOccupiedException.java
@@ -0,0 +1,19 @@
+package CheckTest;
+
+/**
+ * Created by dominicpeart on 11/12/17.
+ */
+public class AlreadyOccupiedException extends RuntimeException
+{
+    public AlreadyOccupiedException(String msg)
+    {
+        super(msg);
+    }
+}
+
+
+
+
+
+
+//https://www.javaworld.com/article/3014190/learn-java/checkers-anyone.html
---
+++ src/CheckTest/CheckerType.java
@@ -0,0 +1,12 @@
+package CheckTest;
+
+/**
+ * Created by dominicpeart on 11/12/17.
+ */
+public enum CheckerType
+{
+    BLACK_REGULAR,
+    BLACK_KING,
+    RED_REGULAR,
+    RED_KING
+}
